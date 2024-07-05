eda8b13202575c7c891a4a186a73adb38279b9ce
Victor Paiva <victor.eagpaiva@gmail.com>
2024-07-05 18:09:17 -0300

    Herança

--- src/ComputadorPedrinho.java
+++ src/ComputadorPedrinho.java
@@ -5,9 +5,20 @@ public class ComputadorPedrinho {
     public static void main(String[] args) {
         //abrindo MSN Messenger
         MSNMessenger msn = new MSNMessenger();
-
+        System.out.println("MSN");
         msn.enviarMensagem();
-
         msn.receberMensagem();
+
+        FacebookMessenger fb = new FacebookMessenger();
+        System.out.println("Facebook");
+        fb.enviarMensagem();
+        fb.receberMensagem();
+
+
+        Telegram tlg = new Telegram();
+        System.out.println("Telegram");
+        tlg.enviarMensagem();
+        tlg.receberMensagem();
+
     }
 }
